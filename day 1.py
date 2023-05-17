ccb4a99e5c8348e4ba60ac99d10856feb8a3b9f8
bravejunior <105417582+bravejunior@users.noreply.github.com>
2023-05-17 23:16:09 +0200

    Including part 2

--- day 1.py
+++ day 1.py
@@ -1,3 +1,4 @@
+##PART 1:
 def get_highest_calories(data):
     highest_calories = 0
     current_elf_calories = 0
@@ -2269,7 +2270,28 @@ data = '''\
 
 '''
 
-print(get_highest_calories(data))
-
 #answer = 68923
 
+############################################################################
+#PART 2:
+def get_highest_calories(data):
+    top_3_highest_calories = [0, 0, 0]
+    total_calories = 0
+    current_elf_calories = 0
+    rows = data.split('\n')
+    for row in rows:
+        if(row):
+            current_elf_calories += int(row)
+        if not (row):
+            if(current_elf_calories > min(top_3_highest_calories)):
+                index = top_3_highest_calories.index(min(top_3_highest_calories))
+                top_3_highest_calories[index] = current_elf_calories
+            current_elf_calories = 0 
+
+    for n in top_3_highest_calories:
+        total_calories += n
+    return total_calories
+
+print(get_highest_calories(data))
+
+#answer = 200044
